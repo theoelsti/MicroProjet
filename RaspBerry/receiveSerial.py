9a0a57e58bbf1951d6303c7faa6a08a6248deec0
Theo <theophile.fouillet@gmail.com>
2020-12-14 14:56:04 +0100

    Added website with temperature, humidity is coming

--- RaspBerry/receiveSerial.py
+++ RaspBerry/receiveSerial.py
@@ -1,16 +1,26 @@
 import serial
-
+import time
 ser = serial.Serial('/dev/ttyACM0',9600, timeout=1)
 ser.flush()
+def tempupdate() :	
+            var1 = ser.readline()	
+            
+            repr(var1)	
+            fob = open('/var/www/html/index.html', 'w')	
+            fob.write('<html><head><meta charset="UTF-8"></head><body><h1>La temperature est de ' + data[1]+ ' à'+ time.strftime('%l:%M%p le %b %d, %Y') + '</h1></body></html>')
+            fob.close()
+
 try:
  ok = True
  while ok:
      if ser.in_waiting >0:
          line = ser.readline().decode('utf-8').rstrip()
          data = line.split(':')
-         jsonPayload = "{" + '"temp":'+ data[0] + ',"hum":'+ data[1] + "}" 
+         jsonPayload = "{" + '"hum":'+ data[0] + ',"temp":'+ data[1] + "}" 
          print(jsonPayload)
-
+         ser = serial.Serial('/dev/ttyACM0', 9600)
+         tempupdate()
+         
 except KeyboardInterrupt:
     print("adios.")
     exit()
