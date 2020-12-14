c9f1e5e99d7bca42c3bf0614ae7b8cf24ca7bcf9
Theo <theophile.fouillet@gmail.com>
2020-12-14 14:08:21 +0100

    finished the arduino codde

--- Arduino/code/code.ino
+++ Arduino/code/code.ino
@@ -1,24 +1,24 @@
-#include <DHT.h>
+#include <DHT.h>;
+
 DHT dht(A0, DHT22);
-float h = dht.readHumidity();
-float t = dht.readTemperature();
-
-
-void setup(){
-
-
-
-}
-void temperature(){
-
-}
-void humidite() {
-
+int chk;
+float hum;  
+float temp;
+void setup()
+{
+  Serial.begin(9600);
+  dht.begin();
 }
 
 void loop()
 {
-humidite();
-temperature();
-delay(30000);
+    hum = dht.readHumidity();
+    temp= dht.readTemperature();   
+    Serial.print("hum:");
+    Serial.print(hum);
+    Serial.print(";temp");
+    Serial.print(temp);
+    delay(2000); 
 }
+
+   
