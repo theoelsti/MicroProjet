4e833d6436fac102e87af01fc4fa611a5ff7bbe6
theoelsti <theophile.fouillet@protonmail.com>
2020-12-15 23:52:04 +0100

    SQL storage added

---
+++ RaspBerry/SQL/pimeteo.sql
@@ -0,0 +1,2 @@
+CREATE TABLE IF NOT EXISTS pimeteo (`date` datetime NOT NULL,
+              temp decimal(3,1) NOT NULL, hum decimal(3,1) NOT NULL) ;
--- RaspBerry/receiveSerial.py
+++ RaspBerry/receiveSerial.py
@@ -1,25 +1,50 @@
 import serial
 import time
+import mysql.connector
+from time import sleep
 ser = serial.Serial('/dev/ttyACM0',9600, timeout=1)
 ser.flush()
-def tempupdate() :	
-            var1 = ser.readline()	
-            
-            repr(var1)	
-            fob = open('/var/www/html/index.html', 'w')	
-            fob.write('<html><head><meta charset="UTF-8"></head><body><h1>La temperature est de ' + data[1]+ '<br> L\'humidité est de '+data[0]+'<br> à'+ time.strftime('%l:%M%p le %b %d, %Y') + '</h1></body></html>')
-            fob.close()
 
+#--------------------------#
+#----Gestion Base Mysql----#
+DB_SERVER ='127.0.0.1' 
+DB_USER='root'     
+DB_PWD=' '          
+DB_BASE='releves'     
+
+def query_db(sql):
+    try:
+        db = mysql.connector.connect(
+            host = DB_SERVER, 
+            user = DB_USER, 
+            password = DB_PWD, 
+            database = DB_BASE) #Connexion
+        cursor = db.cursor() #Curseur
+        cursor.execute(sql) #On envoie la requete et on ferme
+        db.commit()
+        db.close()
+    except:
+        print("Erreur lors de la requete")
+
+def tempget():
+    datebuff = time.strftime('%Y-%m-%d %H:%M:%S')
+    line = ser.readline().decode('utf-8').rstrip()
+    data = line.split(':')
+    jsonPayload = "{" + '"hum":'+ data[0] + ',"temp":'+ data[1] + "}" 
+    print(jsonPayload)
+    query_db("""INSERT INTO pimeteo (date, temp, hum) VALUES ('%s','%s','%s');
+         """ % (datebuff, data[1], data[0]))
+    print("La requete sql a bien été envoyée.")
+         
+#On crée la table si elle n'existe pas
+query_db("""CREATE TABLE IF NOT EXISTS pimeteo (`date` datetime NOT NULL,
+              temp decimal(3,1) NOT NULL, hum decimal(3,1) NOT NULL) ;""")
 try:
  ok = True
  while ok:
-     if ser.in_waiting >0:
-         line = ser.readline().decode('utf-8').rstrip()
-         data = line.split(':')
-         jsonPayload = "{" + '"hum":'+ data[0] + ',"temp":'+ data[1] + "}" 
-         print(jsonPayload)
-         ser = serial.Serial('/dev/ttyACM0', 9600)
-         tempupdate()
+     if ser.in_waiting>0:
+         tempget()
+         sleep(60)
          
 except KeyboardInterrupt:
     print("adios.")
