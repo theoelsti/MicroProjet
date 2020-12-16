60a673a1408d68bbe9c767203b1205e47ff23474
theoelsti <theophile.fouillet@protonmail.com>
2020-12-16 19:20:46 +0100

    Upgraded the fucking generator with sql

---
+++ RaspBerry/SQL/random.sql
@@ -0,0 +1,12 @@
+ INSERT INTO pimeteo (date, temp, hum) VALUES ('2020-12-16 18:58:17','19.4','48.7');
+            INSERT INTO pimeteo (date, temp, hum) VALUES ('2020-12-16 18:58:18','20.3','52.6');
+            INSERT INTO pimeteo (date, temp, hum) VALUES ('2020-12-16 18:58:19','19.2','47.7');
+            INSERT INTO pimeteo (date, temp, hum) VALUES ('2020-12-16 18:58:20','20.1','47.0');
+            INSERT INTO pimeteo (date, temp, hum) VALUES ('2020-12-16 18:58:21','19.9','52.5');
+            INSERT INTO pimeteo (date, temp, hum) VALUES ('2020-12-16 18:58:22','19.5','48.6');
+            INSERT INTO pimeteo (date, temp, hum) VALUES ('2020-12-16 18:58:23','20.7','51.6');
+            INSERT INTO pimeteo (date, temp, hum) VALUES ('2020-12-16 18:58:24','19.9','52.1');
+            INSERT INTO pimeteo (date, temp, hum) VALUES ('2020-12-16 18:58:25','19.7','51.2');
+            INSERT INTO pimeteo (date, temp, hum) VALUES ('2020-12-16 18:58:26','20.1','51.7');
+
+            
--- RaspBerry/generateur.py
+++ RaspBerry/generateur.py
@@ -1,16 +1,27 @@
 import time
 import random
+import mysql.connector
+
+empty="TRUNCATE TABLE `pimeteo`;"
+DB_SERVER ='127.0.0.1' 
+DB_USER='root'     
+DB_PWD=' '          
+DB_BASE='releves'  
+
 tempint = 20.00
 humint = 50.00
-def temp():
 
+def reset_file():
+     sql_file = open('./RaspBerry/SQL/random.sql','w')
+     sql_file.write(" ")
+     sql_file.close
+def temp():
     way = random.randint(0, 1)
     if way:
-        newtemp = tempint + random.uniform(0, 0.5)
+        newtemp = tempint + random.uniform(0, 0.9)
     else:
-        newtemp = tempint - random.uniform(0, 0.5)
-    return newtemp
-
+        newtemp = tempint - random.uniform(0, 0.9)
+    return round(newtemp, 1)
 def hum():
 
     way = random.randint(0, 1)
@@ -18,12 +29,48 @@ def hum():
         newhum = humint + random.uniform(0, 3)
     else:
         newhum = humint - random.uniform(0, 3)
-    return newhum
+    return round(newhum, 1)
+def write(query):
+    sql_file = open('./RaspBerry/SQL/random.sql','a')
+    sql_file.write(query)
+def query_db(sql):
+    try:
+        db = mysql.connector.connect(
+            host = DB_SERVER, 
+            user = DB_USER, 
+            password = DB_PWD, 
+            database = DB_BASE) #Connexion
+        cursor = db.cursor() #Curseur
+        cursor.execute(sql) #On envoie la requete et on ferme
+        db.commit()
+        db.close()
+    except:
+        print("SQL query error")
+def empty_base():
+    try:
+        db = mysql.connector.connect(
+            host = DB_SERVER, 
+            user = DB_USER, 
+            password = DB_PWD, 
+            database = DB_BASE) #Connexion
+        cursor = db.cursor() #Curseur
+        cursor.execute(empty) #On envoie la requete et on ferme
+        db.commit()
+        db.close()
+    except:
+        print("SQL table reset error")
 def generate(nb):
     for i in range(nb):
         datebuff = time.strftime('%Y-%m-%d %H:%M:%S')
         query = """INSERT INTO pimeteo (date, temp, hum) VALUES ('%s','%s','%s');
             """ % (datebuff,temp() ,hum() )
         print(query)
-        time.sleep(10)
-generate(1)
+        write(query)
+        query_db(query)
+        time.sleep(1)
+def setup(nb):
+    reset_file()
+    empty_base()
+    generate(nb)
+generatenumber = int(input("Combien de valeurs souhaitez vous générer ?"))
+setup(generatenumber)
