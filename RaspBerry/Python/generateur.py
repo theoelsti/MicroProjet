84b0b170001755c45268f58f2c1ba412529338e6
theoelsti <theophile.fouillet@protonmail.com>
2020-12-18 21:01:15 +0100

    updated gen

--- RaspBerry/Python/generateur.py
+++ RaspBerry/Python/generateur.py
@@ -1,15 +1,18 @@
 import time
 import random
 import mysql.connector
-
+from datetime import datetime, timedelta
 empty="TRUNCATE TABLE `pimeteo`;"
 DB_SERVER ='127.0.0.1' 
 DB_USER='root'     
 DB_PWD='root'          
 DB_BASE='releves'  
 
+sdate = datetime(2019, 1, 1, 00, 1, 00)
+edate = datetime(2020, 12, 2, 23, 2, 40) 
 tempint = 20.00
 humint = 50.00
+delta = edate - sdate   
 
 def reset_file():
      sql_file = open('../SQL/random.sql','w')
@@ -59,17 +62,19 @@ def empty_base():
         db.close()
     except:
         print("SQL table reset error")
-def generate(nb):
-    for i in range(nb):
-        datebuff = time.strftime('%Y-%m-%d %H:%M:%S')
+def daterange(start_date, end_date):
+    delta = timedelta(seconds=10)
+    while start_date < end_date:
+        yield start_date
+        start_date += delta
+def generate():
+    for single_date in daterange(sdate, edate):
+        timestamp =single_date.strftime('%Y-%m-%d %H:%M:%S')
         query = """INSERT INTO pimeteo (date, temp, hum) VALUES ('%s','%s','%s');
-            """ % (datebuff,temp() ,hum() )
-        write(query)
+                """ % (timestamp,temp() ,hum() )
         query_db(query)
-        time.sleep(1)
-def setup(nb):
-    reset_file()
+def setup():
+    #reset_file()
     empty_base()
-    generate(nb)
-generatenumber = int(input("Combien de valeurs souhaitez vous générer ? "))
-setup(generatenumber)
+    generate()
+setup()
--- RaspBerry/Python/generatorv2.py
+++ RaspBerry/Python/generatorv2.py
@@ -1,5 +1,42 @@
-import datetime
+from datetime import datetime, timedelta
+import random
 
-for i in range(0,20):
-    datebuff = datetime.datetime.now() - datetime.timedelta(hours=i)
-    print(datebuff)
+sdate = datetime(2019, 1, 1, 00, 1, 00)
+edate = datetime(2020, 12, 2, 23, 2, 40) 
+tempint = 20.00
+humint = 50.00
+delta = edate - sdate     
+
+def temp():
+    way = random.randint(0, 1)
+    if way:
+        newtemp = tempint + random.uniform(0, 1.5)
+    else:
+        newtemp = tempint - random.uniform(0, 1.5)
+    return round(newtemp, 1)
+def hum():
+
+    way = random.randint(0, 1)
+    if way:
+        newhum = humint + random.uniform(0, 5)
+    else:
+        newhum = humint - random.uniform(0, 5)
+    return round(newhum, 1)
+def write(query):
+    sql_file = open('RaspBerry/SQL/longrandom.sql','a')
+    sql_file.write(query)
+
+def daterange(start_date, end_date):
+    delta = timedelta(seconds=10)
+    while start_date < end_date:
+        yield start_date
+        start_date += delta
+
+def generate():
+
+    for single_date in daterange(sdate, edate):
+        timestamp =single_date.strftime('%Y-%m-%d %H:%M:%S')
+        query = """INSERT INTO pimeteo (date, temp, hum) VALUES ('%s','%s','%s');
+                """ % (timestamp,temp() ,hum() )
+        write(query)
+generate()
