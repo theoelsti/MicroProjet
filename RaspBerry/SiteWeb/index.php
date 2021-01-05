ecc31a9b74fde30f5617d42c4a5bfd5dee827998
Femboy <theophile.fouillet@gmail.com>
2021-01-05 12:28:40 +0100

    No more space

--- RaspBerry/SiteWeb/index.php
+++ RaspBerry/SiteWeb/index.php
@@ -104,7 +104,7 @@
 function updateSQL(){
     $param = $_GET['param'];
     $show = $_GET['show'];
-    $link = mysqli_connect("localhost:3306", "root", "", "releves");
+    $link = mysqli_connect("localhost:3306", "root", "root", "releves");
     if ($link->connect_errno) {
         echo "Echec lors de la connexion à mysqli : (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
     }
