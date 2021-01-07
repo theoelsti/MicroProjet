f55790945f6ff8415238332af7ee9cdb3ca8ac44
Compooter <theophile.fouillet@protonmail.com>
2021-01-07 03:22:44 +0100

    kill me

--- RaspBerry/Python/test_month.py
+++ RaspBerry/Python/test_month.py
@@ -6,22 +6,30 @@ mydb = mysql.connector.connect(
   password="root",
   database="releves"
 )
-
+def _sum(arr):  
+    sum=0
+    for i in arr: 
+        sum = sum + i 
 mycursor = mydb.cursor()
 
 mycursor.execute("SELECT date FROM pimeteo")
 
 myresult = mycursor.fetchall()
 dates = [] 
+tempsum = 0
 for x in myresult:
     for d in x:
+        temps = [] 
         if(oldTime != str(d)[0:10]):
-            tempshum = mycursor.execute("SELECT temp, hum FROM pimeteo where date like \"%" + str(d)[0:10] + "%\";")
+            tempshum = mycursor.execute("SELECT temp FROM pimeteo where date like \"%" + str(d)[0:10] + "%\";")
             tempshum = mycursor.fetchall()
-           
+            length = len(tempshum)
+            
             for d in tempshum:
-                print(d[0])
-
-            dates.append(str(d)[0:10])
+                tempsum += float(d[0])
+            print(tempsum / length)
+            
+        dates.append(str(d)[0:10])
         oldTime = str(d)[0:10]
+    tempsum = 0
 print(len(dates))
