ed1fd71750f5ad3a9bc4ceb0b25f61c2a48b2303
Compooter <theophile.fouillet@protonmail.com>
2021-01-10 23:13:58 +0100

    Finished median page

--- RaspBerry/SiteWeb/median.php
+++ RaspBerry/SiteWeb/median.php
@@ -86,30 +86,30 @@ onmousedown="return false;">
 <div class="buttonX">
     <div class ="buttonXpos">
         </div> 
-        <div id="humday"  class="values-container temperature" > 7 jours</div>
-        <div id="resday"  class="values-container humidity" > 31 jours</div>
+        <div  class="values-container temperature" > <div  style="margin-top:15px;font-size:50px;">7 jours</div> </div>
+        <div  class="values-container humidity" >  <div  style="  font-size:50px;margin-top:15px">31 jours</div></div>
     </div>
 </div>  
 <br>
 <div class="buttonY">
     <div class ="buttonYpos">
         </div> 
-        <div id="humday"  class="values-container temperature" > Temperature</div>
-        <div id="resday"  class="values-container humidity" > Temperature</div>
+        <div  class="values-container temperature " > Temperature <div class="tempweek"></div></div>
+        <div   class="values-container humidity " > Temperature <div class="tempmonth"></div></div>
     </div>
 </div>  
 <br>
 <div class="buttonW"> 
     <div class ="buttonYpos">
-        <div  id="tempweek" class="values-container temperature"> Temperature ressenti</div>
-        <div  id="humweek"  class="values-container humidity" > Temperature ressenti</div>
+        <div  class="values-container temperature "> Temperature ressentie <div class="resweek"></div></div>
+        <div   class="values-container humidity " > Temperature ressentie <div class="resmonth"></div></div>
     </div>
 </div>  
 <br>
 <div class="buttonZ"> 
     <div class ="buttonYpos">
-        <div  id="tempweek" class="values-container temperature"> Humidite</div>
-        <div  id="humweek"  class="values-container humidity" > Humidite</div>
+        <div  class="values-container temperature "> Humidite <div class="humweek"></div></div>
+        <div  class="values-container humidity " > Humidite <div class="hummonth"></div></div>
     </div>
 </div>
 
@@ -160,13 +160,14 @@ function day(){
         $oldstamp = "";
         $datesok = [];
         
-        for($d = sizeof($date)-1; $d > sizeof($date)-144; $d--){
+        for($d = sizeof($date)-1; $d > sizeof($date)-1008; $d--){
             if($oldstamp == substr($date[$d], 0, -6)){
                 $oldstamp = substr($date[$d], 0, -6);
             }
             else{
                 $oldstamp = substr($date[$d], 0, -6); // Nouvelle date
                 array_push($datesok, $oldstamp);
+                
             }
         }
         
@@ -174,9 +175,10 @@ function day(){
         
         
         $temptab = [];
+        
         echo "\n";
-        echo "temprawday   = ";
-        for($d = $datesize; $d > $datesize-24; $d--){
+        echo "tempMedian   = ";
+        for($d = $datesize; $d >0; $d--){
             //1 nouveau jour unique
                     $sql = "SELECT * FROM pimeteo " . "where date like \"%" . $datesok[$d] . "%\";";
                     if($result = mysqli_query($link, $sql)){
@@ -197,8 +199,8 @@ function day(){
         
         echo "\n";
         $humtab = [];
-        echo "humrawday  = ";
-        for($d = $datesize; $d > $datesize-24; $d--){
+        echo "humMedian  = ";
+        for($d = $datesize; $d > 0; $d--){
             //1 nouveau jour unique
                     $sql = "SELECT * FROM pimeteo " . "where date like \"%" . $datesok[$d] . "%\";";
                     if($result = mysqli_query($link, $sql)){
@@ -220,8 +222,8 @@ function day(){
             
             echo "\n";
             $restab = [];
-            echo "resrawday   = ";
-            for($d = $datesize; $d > $datesize-24; $d--){
+            echo "resMedian   = ";
+            for($d = $datesize; $d > 0; $d--){
             //1 nouveau jour unique
                         $sql = "SELECT * FROM pimeteo " . "where date like \"%" . $datesok[$d] . "%\";";
                         if($result = mysqli_query($link, $sql)){
@@ -244,6 +246,115 @@ function day(){
     
 }
 day();
+
+function week(){
+    $link = mysqli_connect("localhost:3306", "root", "root", "releves");
+    if ($link->connect_errno) {
+        echo "Echec lors de la connexion à mysqli : (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+    }
+    
+        
+    $query = "SELECT * FROM pimeteo;";
+    $sql =  $query ;
+    $temp = [];
+    $hum = [];
+    if($result = mysqli_query($link, $sql)){
+        if(mysqli_num_rows($result) > 0){
+            while($row = mysqli_fetch_array($result)){
+                $date[] = $row['date']; 
+            }
+        
+        
+        
+        $oldstamp = "";
+        $datesok = [];
+        
+        for($d = sizeof($date)-1; $d > 4464; $d--){
+            if($oldstamp == substr($date[$d], 0, -6)){
+                $oldstamp = substr($date[$d], 0, -6);
+            }
+            else{
+                $oldstamp = substr($date[$d], 0, -6); // Nouvelle date
+                array_push($datesok, $oldstamp);
+                
+            }
+        }
+        
+        $datesize = sizeof($datesok)-1;
+        
+        
+        $temptab = [];
+        echo "\n";
+        echo "tempMonthMedian   = ";
+        for($d = $datesize; $d > 0; $d--){
+            //1 nouveau jour unique
+                    $sql = "SELECT * FROM pimeteo " . "where date like \"%" . $datesok[$d] . "%\";";
+                    if($result = mysqli_query($link, $sql)){
+                        if(mysqli_num_rows($result) > 0){
+                
+                            while($row = mysqli_fetch_array($result)){ 
+                                $temp[] = $row['temp'];   
+                            }
+                            array_push($temptab, bcdiv(array_sum($temp) / count($temp), 1, 2));
+                        }
+                    }
+                    $temp = [];  
+            }
+            echo bcdiv(array_sum($temptab) / count($temptab), 1, 2);
+            echo "; ";
+        
+        }
+        
+        echo "\n";
+        $humtab = [];
+        echo "humMonthMedian  = ";
+        for($d = $datesize; $d > 0; $d--){
+            //1 nouveau jour unique
+                    $sql = "SELECT * FROM pimeteo " . "where date like \"%" . $datesok[$d] . "%\";";
+                    if($result = mysqli_query($link, $sql)){
+                        if(mysqli_num_rows($result) > 0){
+                
+                            while($row = mysqli_fetch_array($result)){ 
+                                $hum[] = $row['hum'];   
+                            }
+                            array_push($humtab,bcdiv(array_sum($hum) / count($hum), 1, 2));
+                            
+                        }
+                    }
+                    $hum = [];  
+            }
+            echo bcdiv(array_sum($humtab) / count($humtab), 1, 2);
+            echo ";";
+
+
+            
+            echo "\n";
+            $restab = [];
+            echo "resMonthMedian   = ";
+            for($d = $datesize; $d > 0; $d--){
+            //1 nouveau jour unique
+                        $sql = "SELECT * FROM pimeteo " . "where date like \"%" . $datesok[$d] . "%\";";
+                        if($result = mysqli_query($link, $sql)){
+                            if(mysqli_num_rows($result) > 0){
+                    
+                                while($row = mysqli_fetch_array($result)){ 
+                                    $res[] = $row['res'];   
+                                }
+                                array_push($restab,bcdiv(array_sum($res) / count($res), 1, 2));
+                                
+                            }
+                        }
+                        $res = [];  
+            }
+            echo(bcdiv(array_sum($restab) / count($restab), 1, 2));
+            echo "; ";
+        $time = $temp  =$hum = $row = $tempsize = $humsize  = $timesize = $res = 0;
+    }
+    
+    
+}
+week();
+
 getTotalValues()
 ?>
 
--- RaspBerry/SiteWeb/scripts/medianjs.js
+++ RaspBerry/SiteWeb/scripts/medianjs.js
@@ -1,6 +1,12 @@
 
 var totalValuesField = document.getElementsByClassName("lastvalues")[0];
 var dateField = document.getElementsByClassName("firstdate")[0];
+var tempWeekField = document.getElementsByClassName("tempweek")[0]
+var humWeekField = document.getElementsByClassName("humweek")[0]
+var resWeekField = document.getElementsByClassName("resweek")[0]
+var tempMonthField = document.getElementsByClassName("tempmonth")[0]
+var humMonthField = document.getElementsByClassName("hummonth")[0]
+var resMonthField = document.getElementsByClassName("resmonth")[0]
 let t = 0;
 let humdone = false;
 
@@ -13,7 +19,7 @@ function countForTotal(){
             else t += 10;
         }
         else t += 121;
-    totalValuesField.innerHTML = " <span title='Valeurs totales relevées' id=\"totalvalues\" >" + t + "</span>" ;
+    totalValuesField.innerHTML = " <span title='Valeurs totales relevées' class=\"totalvalues\" >" + t + "</span>" ;
    
     }
     else{
@@ -24,7 +30,6 @@ function countForTotal(){
 let t1 = 0
 let t2 = 0
 let t3 = 0
-
 let splittedtab = mostoldDate.split("-")
 let hourstab = splittedtab[2].split(" ")
 let y = splittedtab[0]
@@ -47,7 +52,93 @@ function countDate(){
         window.clearInterval(dateFunction);
     }
 }
-// Calcule la moyenne des tableaux
+
+
+let tw = 0.0
+let hw = 0.0
+let resw = 0.0
+// Compte pour la semaine
+    // week
+        var TempweekFunction = self.setInterval(function () {countForTempWeek()}, 4);
+        function countForTempWeek(){    
+            if(tw <= tempMedian){
+
+                tw += 0.1;
+                tempWeekField.innerHTML = " <span title='Température moyenne de la semaine' class=\"totalvalues\" >" + tw.toFixed(1) +" °C </span>" ;
+
+            }else{
+
+                window.clearInterval(TempweekFunction);
+            }
+        }
+        var humweekFunction = self.setInterval(function () {countForhumWeek()}, 4);
+        function countForhumWeek(){    
+            if(hw <= humMedian){
+
+                hw += 0.1;
+                humWeekField.innerHTML = " <span title='Humidité moyenne de la semaine' class=\"totalvalues\" >" + hw.toFixed(1) +" % </span>" ;
+
+            }else{
+
+                window.clearInterval(TempweekFunction);
+            }
+        }
+        var resweekFunction = self.setInterval(function () {countForresWeek()}, 4);
+        function countForresWeek(){    
+            if(resw <= resMedian){
+
+                resw += 0.1;
+                resWeekField.innerHTML = " <span title='Humidité moyenne de la semaine' class=\"totalvalues\" >" + resw.toFixed(1) +" °C </span>" ;
+
+            }else{
+
+                window.clearInterval(TempweekFunction);
+            }
+        }
+
+
+
+let tm = 0.0
+let hm = 0.0
+let resm = 0.0
+    //Mois
+        // Month
+    var TempMonthFunction = self.setInterval(function () {countForTempMonth()}, 4);
+    function countForTempMonth(){    
+        if(tm <= tempMonthMedian){
+
+            tm += 0.1;
+            tempMonthField.innerHTML = " <span title='Température moyenne de la semaine' class=\"totalvalues\" >" + tm.toFixed(1) +" °C </span>" ;
+
+        }else{
+
+            window.clearInterval(TempMonthFunction);
+        }
+    }
+    var humMonthFunction = self.setInterval(function () {countForhumMonth()}, 4);
+    function countForhumMonth(){    
+        if(hm <= humMonthMedian){
+
+            hm += 0.1;
+            humMonthField.innerHTML = " <span title='Humidité moyenne de la semaine' class=\"totalvalues\" >" + hm.toFixed(1) +" % </span>" ;
+
+        }else{
+
+            window.clearInterval(TempMonthFunction);
+        }
+    }
+    var resMonthFunction = self.setInterval(function () {countForresMonth()}, 4);
+    function countForresMonth(){    
+        if(resm <= resMonthMedian){
+
+            resm += 0.1;
+            resMonthField.innerHTML = " <span title='Humidité moyenne de la semaine' class=\"totalvalues\" >" + resm.toFixed(1) +" °C </span>" ;
+
+        }else{
+
+            window.clearInterval(TempMonthFunction);
+        }
+    }
 
 
 // 0xFadeath Copyrigthed this
